8dae0ef2a45d43c9672bba2e04bc570f38bb2fd8
John <Jane>
2023-05-16 17:08:00 +0200

    derniere modification

--- js/page2023en.js
+++ js/page2023en.js
@@ -34,34 +34,39 @@ $(document).ready(function(){
     }
   }
   document.addEventListener("DOMContentLoaded", function() {
-
-  
-    console.log(document.getElementById("myBtn"));
-    
-    // Obtenez la popup
     var modal = document.getElementById("myModal");
-    
-    // Obtenez le bouton qui ouvre la popup
     var btn = document.getElementById("myBtn");
-    
-    // Obtenez le bouton de fermeture de la popup
     var span = document.getElementsByClassName("close")[0];
-    
-    // Lorsque l'utilisateur clique sur le bouton, ouvrez la popup
+  
     btn.onclick = function() {
       modal.style.display = "block";
     }
-    
-    // Lorsque l'utilisateur clique sur le bouton de fermeture, fermez la popup
+  
     span.onclick = function() {
       modal.style.display = "none";
     }
-    
-    // Lorsque l'utilisateur clique en dehors de la popup, fermez-la
+  
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
     }
-    });
-    
+  
+    var moda = document.getElementById("myModa");
+    var bt = document.getElementById("myBt");
+    var spa = document.getElementsByClassName("clos")[0];
+  
+    bt.onclick = function() {
+      moda.style.display = "block";
+    }
+  
+    spa.onclick = function() {
+      moda.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == moda) {
+        moda.style.display = "none";
+      }
+    }
+  });
